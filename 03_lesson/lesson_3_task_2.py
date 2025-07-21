d1ab1ae8ae7330ae032359f18abb26d066f14d7a
davydova-katerina <davydova.katerina84@gmail.com>
2025-07-22 01:28:46 +0300

    03_lesson

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -1,11 +1,11 @@
 from smartphone import Smartphone
 
 catalog = [
-    Smartphone("IPhone", "15Pro", +79123456789),
-    Smartphone("Samsung", "A3", +79234567891),
-    Smartphone("LG", "Razer", +79345678912),
-    Smartphone("Sony", "Q1", +79456789123),
-    Smartphone("Honor", "XE", +79567891234)
+    Smartphone("IPhone", "15Pro", "+79123456789"),
+    Smartphone("Samsung", "A3", "+79234567891"),
+    Smartphone("LG", "Razer", "+79345678912"),
+    Smartphone("Sony", "Q1", "+79456789123"),
+    Smartphone("Honor", "XE", "+79567891234")
 ]
 
 for smartphone in catalog:
