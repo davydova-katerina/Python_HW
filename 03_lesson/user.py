a8302add51fe68b5935a24e960931868f98786a2
davydova-katerina <davydova.katerina84@gmail.com>
2025-07-21 21:45:48 +0300

    03_lesson

---
+++ 03_lesson/user.py
@@ -0,0 +1,21 @@
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def print_first_name(self):
+        print(self.first_name)
+
+    def print_last_name(self):
+        print(self.last_name)
+
+    def print_full_name(self):
+        print(f"{self.first_name} {self.last_name}")
+
+Ekaterina = User("Ekaterina", "Davydova")
+Davydova = User("Ekaterina", "Davydova")
+Ekaterina_Davydova = User("Ekaterina", "Davydova")
+
+Ekaterina.print_first_name()
+Ekaterina.print_last_name()
+Ekaterina.print_full_name()
