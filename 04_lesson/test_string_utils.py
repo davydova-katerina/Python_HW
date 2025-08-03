281dd277d67eced887a0a39d4b4329e17d7121fc
davydova-katerina <davydova.katerina84@gmail.com>
2025-08-03 22:12:06 +0300

    03_lesson

--- 04_lesson/test_string_utils.py
+++ 04_lesson/test_string_utils.py
@@ -40,3 +40,50 @@ def test_trim_positive(input_str, expected):
 def test_trim_negative(input_str, expected):
     assert string_utils.trim(input_str) == expected
 
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol", [
+    ("Skypro", "S"),
+    ("Skypro", "y"),
+    ("Skypro", "r"),
+])
+def test_contains_positive(input_str, symbol):
+    assert string_utils.contains("Skypro", "S") == True
+    assert string_utils.contains("Skypro", "y") == True
+    assert string_utils.contains("Skypro", "r") == True
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol", [
+    ("Skypro", "T"),
+    ("Skypro", "1"),
+    ("Skypro", "x"),
+])
+def test_contains_negative(input_str, symbol):
+    assert string_utils.contains("Skypro", "T") == False
+    assert string_utils.contains("Skypro", "1") == False
+    assert string_utils.contains("Skypro", "x") == False
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol", [
+    ("SkyPro", "S"),
+    ("SkyPro", "k"),
+    ("SkyPro", "P"),
+])
+def test_delete_symbol_positive(input_str, symbol,):
+    assert string_utils.delete_symbol("SkyPro", "S") == "kyPro"
+    assert string_utils.delete_symbol("SkyPro", "k") == "SyPro"
+    assert string_utils.delete_symbol("SkyPro", "P") == "Skyro"
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol", [
+    ("SkyPro", "Z"),
+    ("SkyPro", "1"),
+    ("SkyPro", ""),
+])
+def test_delete_symbol_negative(input_str, symbol,):
+    assert string_utils.delete_symbol("SkyPro", "Z") == "SkyPro"
+    assert string_utils.delete_symbol("SkyPro", "1") == "SkyPro"
+    assert string_utils.delete_symbol("SkyPro", "") == "SkyPro"
