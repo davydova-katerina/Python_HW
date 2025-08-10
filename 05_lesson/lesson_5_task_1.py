974c1e3504ff0629ec93cc988ec42542a1ea0776
davydova-katerina <davydova.katerina84@gmail.com>
2025-08-10 23:05:11 +0300

    05_lesson

---
+++ 05_lesson/lesson_5_task_1.py
@@ -0,0 +1,17 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
+
+driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("http://uitestingplayground.com/classattr")
+
+blue_button = driver.find_element(By.CSS_SELECTOR, "button.class1.btn-primary.btn-test")
+blue_button.click()
+
+sleep(5)
