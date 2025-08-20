829ebd104ec39eca794ff2c0bc47fa8c582dd9b7
davydova-katerina <davydova.katerina84@gmail.com>
2025-08-20 22:23:33 +0300

    07_lesson

--- 07_lesson/lesson_07_shop_page.py
+++ 07_lesson/lesson_07_shop_page.py
@@ -29,24 +29,28 @@ class LoginPage:
     def click_login(self):
         login_button = self.driver.find_element(By.ID, "login-button")
         login_button.click()
-        return InventoryPage(self.driver)
+        return ProductsPage(self.driver)
 
 
-class InventoryPage:
+class ProductsPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def add_item_to_cart(self, item_name):
-        item_xpath = f"//div[text()='{item_name}']/ancestor::div[@class='inventory_item']//button"
-        add_button = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, item_xpath))
-        )
-        add_button.click()
+    def add_product_to_cart(self, product_name):
+        product_map = {
+            "Sauce Labs Backpack": "add-to-cart-sauce-labs-backpack",
+            "Sauce Labs Bolt T-Shirt": "add-to-cart-sauce-labs-bolt-t-shirt",
+            "Sauce Labs Onesie": "add-to-cart-sauce-labs-onesie"
+        }
+
+        if product_name in product_map:
+            add_button = self.driver.find_element(By.ID, product_map[product_name])
+            add_button.click()
         return self
 
     def go_to_cart(self):
-        cart_button = self.driver.find_element(By.CLASS_NAME, "shopping_cart_link")
-        cart_button.click()
+        cart_icon = self.driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+        cart_icon.click()
         return CartPage(self.driver)
 
 
@@ -54,9 +58,9 @@ class CartPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def get_cart_items(self):
-        cart_items = self.driver.find_elements(By.CLASS_NAME, "cart_item")
-        return [item.text for item in cart_items]
+    def get_cart_items_count(self):
+        cart_badge = self.driver.find_elements(By.CLASS_NAME, "shopping_cart_badge")
+        return int(cart_badge[0].text) if cart_badge else 0
 
     def click_checkout(self):
         checkout_button = self.driver.find_element(By.ID, "checkout")
@@ -69,10 +73,6 @@ class CheckoutPage:
         self.driver = driver
 
     def fill_shipping_info(self, first_name, last_name, postal_code):
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.ID, "first-name"))
-        )
-
         first_name_field = self.driver.find_element(By.ID, "first-name")
         first_name_field.clear()
         first_name_field.send_keys(first_name)
@@ -92,8 +92,13 @@ class CheckoutPage:
         continue_button.click()
         return self
 
-    def get_total_amount(self):
+    def get_total_price(self):
         total_element = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "summary_total_label"))
         )
         return total_element.text.split("$")[1]
+
+    def finish_checkout(self):
+        finish_button = self.driver.find_element(By.ID, "finish")
+        finish_button.click()
+        return self
