8319f6507e45f9ee72c987adfce414e06867f98d
davydova-katerina <davydova.katerina84@gmail.com>
2025-09-15 17:39:35 +0300

    10_lesson

--- 10_lesson/CartPage.py
+++ 10_lesson/CartPage.py
@@ -3,7 +3,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from typing import Tuple, Union
+from typing import Tuple, List
 import allure
 
 class CartPage(Page):
