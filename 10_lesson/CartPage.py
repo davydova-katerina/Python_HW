4cf447c78db26d7004cb7de66bb8e8ff888bec9e
davydova-katerina <davydova.katerina84@gmail.com>
2025-09-15 17:32:19 +0300

    10_lesson

--- 10_lesson/CartPage.py
+++ 10_lesson/CartPage.py
@@ -1,3 +1,11 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from typing import Tuple, Union
+import allure
+
 class CartPage(Page):
     """Страница корзины Sauce Demo."""
 
